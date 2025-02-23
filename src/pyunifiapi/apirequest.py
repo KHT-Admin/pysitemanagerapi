164eb477398d2a0c072d6f76cee3a4591eff05fd
KHT-Admin <171313330+KHT-Admin@users.noreply.github.com>
2025-02-24 08:01:14 +1100

    Initial Commit

---
+++ src/pyunifiapi/apirequest.py
@@ -0,0 +1,71 @@
+import http.client
+import json
+from dataclasses import dataclass
+
+from . import errors
+
+API_HOST = "api.ui.com"
+API_VERSION = "0.1"
+
+
+@dataclass
+class UniFiApiResponse:
+    code: str = None
+    data: dict = None
+    httpStatusCode: int = None
+    message: str = None
+    traceId: str = None
+
+
+class api_api_request:
+    """Context manager for API requests."""
+
+    def __init__(self, connection, method: str, url: str, headers: dict):
+        self.conn = connection
+        self.conn.request(method, url, headers=headers)
+
+    def __enter__(self):
+        return self.conn
+
+    def __exit__(self, exception_type, exception, traceback):
+        self.conn.close()
+
+
+class UniFiApiRequest:
+    def __init__(self, api_key: str, early_access: bool):
+        self._headers: dict = {
+            "X-API-KEY": api_key,
+            "Accept": "application/json",
+        }
+
+        self._base_path = "/ea" if early_access else ""
+
+        try:
+            self.conn = http.client.HTTPSConnection(API_HOST)
+
+        except Exception as e:
+            raise e
+
+    def _api_request(self, method, url):
+        with api_api_request(self.conn, method, url, self._headers) as r:
+            resp = r.getresponse()
+            response = UniFiApiResponse(**json.loads(resp.read()))
+
+        match response.httpStatusCode:
+            case 200:
+                return response.data
+            case 400:
+                raise errors.InvalidParameter(response.message)
+            case 401:
+                raise errors.Unauthorised(response.message)
+            case 404:
+                raise errors.NotFound(response.message)
+            case 429:
+                raise errors.RateLimited(response.message)
+            case 500:
+                raise errors.ServerError(response.message)
+            case 502:
+                raise errors.BadGateway(response.message)
+
+    def get(self, url: str):
+        return self._api_request("GET", f"{self._base_path}/{url}")
---
+++ tests/test_client.py
@@ -0,0 +1,62 @@
+import dataclasses
+import json
+import os
+import pprint
+
+import pytest
+from pyunifiapi.apirequest import UniFiApiRequest
+from pyunifiapi.client import UniFiApiClient
+from pyunifiapi.errors import NotFound, UniFiApiError
+
+_UNIFI_API_KEY = os.environ["UNIFI_API_KEY"]
+_N_SITES = 2
+
+
+def test_client_unifi():
+    a = UniFiApiClient("mykey")
+    assert isinstance(a, UniFiApiClient)
+
+
+def test_client_apirequest_init():
+    a = UniFiApiRequest("abc", True)
+    assert isinstance(a, UniFiApiRequest)
+
+
+def test_client_apirequest_validate_api_request():
+    with pytest.raises(Exception) as e_info:
+        a = UniFiApiRequest("request")
+
+
+def test_list_sites():
+    a = UniFiApiClient(_UNIFI_API_KEY, True)
+    b = a.list_sites()
+    _N_SITES = len(b)
+    assert True
+
+
+def test_client_api_request():
+    a = UniFiApiClient(_UNIFI_API_KEY, True)
+    bs = a.list_hosts()
+    assert len(bs) == _N_SITES
+
+
+def test_bad_hostid():
+    a = UniFiApiClient(_UNIFI_API_KEY, True)
+    with pytest.raises(NotFound) as e_info:
+        bs = a.get_host_by_id("0000")
+
+
+def test_host_api_request():
+    a = UniFiApiClient(_UNIFI_API_KEY, True)
+    bs = a.list_hosts()[1]
+
+
+a = UniFiApiClient(_UNIFI_API_KEY, True)
+hosts = a.list_hosts()
+pprint.pp(hosts)
+
+# hostids = a.list_host_ids()
+# devs = a.list_devices(hostids.get("officeKHT"))
+
+# for dev in devs[0]["devices"]:
+#     print(f"{dev.get('model')}", {dev.get('name')}, {dev.get('version')}"")
